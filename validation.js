b920a9af152905a65b9db0c13f7e0755de93f574
jontyxo <65168230+jontyxo@users.noreply.github.com>
2022-11-10 03:18:00 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -5,17 +5,17 @@ const Joi = require('@hapi/joi');
 
 const registerValidation =(data) => {
     const schema=Joi.object({
-        name:Joi.string().min(6).required(),
-        email:Joi.string().min(6).required().email(),
-        password:Joi.string().min(6).required()
+        name:Joi.string().min(3).required(),
+        email:Joi.string().min(3).required().email(),
+        password:Joi.string().min(3).required()
     });
 return schema.validate(data)  
 }
 
 const loginValidation =(data) => {
     const schema=Joi.object({
-        email:Joi.string().min(6).required().email(),
-        password:Joi.string().min(6).required()
+        email:Joi.string().min(3).required().email(),
+        password:Joi.string().min(3).required()
     });
 return schema.validate(data)  
 }
